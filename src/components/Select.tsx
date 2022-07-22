929056491ed054cffe974fab0c24e9b335f33d17
Guido Schmidt <git@guidoschmidt.cc>
2022-07-22 12:44:34 +0200

    ADD/src/components/Select

---
+++ src/components/Select.tsx
@@ -0,0 +1,49 @@
+import { createSignal } from "solid-js";
+import { For, Show } from "solid-js/web";
+import "scss.ui.toolkit/scss/components/_select.scss";
+
+interface ISelectProps {
+  options: Array<T>;
+  onSelect?: (v: T, i?: number) => void;
+  formatOption?: (o: T, i?: number) => JSX.Element;
+}
+
+function Select(props: ISelectProps) {
+  const [selected, setSelected] = createSignal(props.options[0]);
+  const [isOpen, setIsOpen] = createSignal(false);
+  const [options] = createSignal(props.options);
+
+  const onSelectOption = (option: any, index: number) => {
+    setSelected(option);
+    setIsOpen(false);
+    props.onSelect && props.onSelect(option, index());
+  };
+
+  return (
+    <div class="select">
+      <div class="selected" onClick={() => setIsOpen(!isOpen())}>
+        {props.formatOption ? props.formatOption(selected()) : selected()}
+      </div>
+      <Show when={isOpen()}>
+        <div class="options">
+          <For each={options()} fallback={<></>}>
+            {(option, index) => {
+              return (
+                <div
+                  class="option"
+                  onClick={() => onSelectOption(option, index)}
+                >
+                  {props.formatOption
+                    ? props.formatOption(option, index)
+                    : option}
+                </div>
+              );
+            }}
+          </For>
+        </div>
+      </Show>
+    </div>
+  );
+}
+
+export { Select };
