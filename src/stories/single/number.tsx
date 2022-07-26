f63eb4c338c42a63d49ce4aa5b29be436be9f980
Guido Schmidt <git@guidoschmidt.cc>
2022-07-26 08:43:45 +0200

    EDIT/stories/number

--- src/stories/single/number.tsx
+++ src/stories/single/number.tsx
@@ -1,11 +1,38 @@
-import { NumberInput, Label } from "../../components";
+import { createStore } from "solid-js/store";
+import { NumberInput, Label, LabelOrientatian } from "../../components";
 
 const NumberInputStory = () => (
-  <Label text="Numerical Input">
+  <Label text="Numerical Input" orientation={LabelOrientatian.COLUMN}>
     <NumberInput value={100} onChange={() => {}} />
   </Label>
 );
 
 NumberInputStory.storyName = "Numbper Input";
 
-export { NumberInputStory };
+const ArrayNumberInputStory = () => {
+  const [values, updateValues] = createStore(
+    new Array(200).fill(0).map((_) => Math.round(Math.random() * 500))
+  );
+
+  return (
+    <Label
+      text="Numerical Input"
+      orientation={LabelOrientatian.COLUMN}
+      style={{ display: "grid", "grid-template-columns": "repeat(5, auto)" }}
+    >
+      {values.map((v: number, i: number) => {
+        return (
+          <NumberInput
+            value={v}
+            onChange={(nv: number) => updateValues(i, nv)}
+          />
+        );
+      })}
+    </Label>
+  );
+};
+
+NumberInputStory.storyName = "Numbper Input";
+ArrayNumberInputStory.storyName = "Array<Number Input>";
+
+export { NumberInputStory, ArrayNumberInputStory };
